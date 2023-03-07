4517b1ef4d8dc8d554198638fe4d52fb4baaa1ee
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-07 16:07:01 +0300

    Componentler Router'e Baglanıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import {Route,Routes} from "react-router-dom";
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+import { Menu } from './Components/Menu';
+import { Contact } from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
-      Vepa
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' exact element={<About/>} />
+        <Route path='/menu' exact element={<Menu/>} />
+        <Route path='/contact' exact element={<Contact/>} />
+
+      </Routes>
     </div>
   );
 }
