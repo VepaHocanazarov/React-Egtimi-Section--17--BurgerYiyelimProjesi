e31bf580483d1c4438e44127e2e0d2a64fe85866
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-08 16:06:22 +0300

    202. İletişim Sayfası Oluşturuldu ve Proje Bitti

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
+import BannerImage from "../Images/banner.png";
+import "../Styles/Contact.css"
+
+
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+
+      <div className="leftSide" style={{ backgroundImage: `url(${BannerImage})` }}></div>
+
+
+      <div className="rightSide">
+        <h1 className='contactTitle'>Bizimle İletişime Geçin</h1>
+        <form>
+          <label >Ad Soyad</label>
+          <input type="text" name='name' placeholder='Lütfen Adınızı Soaydınızı Giriniz...' />
+
+          <label >Email</label>
+          <input type="email" name='email' placeholder='Lütfen Emailinizi Giriniz...' />
+
+          <label >Mesajınız</label>
+          <textarea name='message' rows={6} placeholder='Lütfen Adınızı Soaydınızı Giriniz...'></textarea>
+          
+          <button>Gönder</button>
+        </form>
+
+        
+      </div>
+
+    </div>
   )
 }
