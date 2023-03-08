b055be090ce7b809c50fe50b33bd9135031b98ef
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-08 09:11:22 +0300

    199. Anasayfa Oluşturuldu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import BannerImage from "../Images/banneryeni.jpg";
+import "../Styles/Home.css";
+
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='mainPage' style={{backgroundImage:`url(${BannerImage})`}}>
+      <div className='order'>
+        <Link to="/menu">
+          <button>SİPARİŞ VER</button>
+        </Link>
+      </div>
+    </div>
   )
 }
