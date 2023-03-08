ff3e2939deedbc90e0077d406a3622a4fb90e42f
Vepa Hocanazarov <vefa00055@gmail.com>
2023-03-08 13:15:14 +0300

    201. Hakkımızda Sayfası Oluşturuldu

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import BannerImage from "../Images/banneryeni.jpg";
+import "../Styles/About.css"
+
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+
+      <div className='aboutTop' style={{ backgroundImage: `url(${BannerImage})` }}></div>
+      <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nesciunt repudiandae sunt ullam delectus reiciendis perferendis commodi tempore aut totam consequatur ut impedit, eaque temporibus qui.
+          Ad quaerat cumque dolorum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores perferendis fuga, tempore unde exercitationem vero commodi id animi, necessitatibus labore odio! Molestiae rem maxime quae tenetur dolorum recusandae amet.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero culpa voluptate maxime, perspiciatis commodi. Voluptatibus aut sint ducimus in molestias mollitia sed deleniti hic animi. Minima ut eum ducimus.
+        </p>
+      </div>
+
+    </div>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,11 +6,14 @@ import "../Styles/Home.css";
 
 export const Home = () => {
   return (
-    <div className='mainPage' style={{backgroundImage:`url(${BannerImage})`}}>
-      <div className='order'>
-        <Link to="/menu">
-          <button>SİPARİŞ VER</button>
-        </Link>
+
+    <div className='container'>
+      <div className='mainPage' style={{ backgroundImage: `url(${BannerImage})` }}>
+        <div className='order'>
+          <Link to="/menu">
+            <button>SİPARİŞ VER</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
